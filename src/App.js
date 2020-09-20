9eae5d35aa14999876efa56b93143f2df3d988bb
Maxim Umblia <umblia34@gmail.com>
2020-09-20 18:03:09 +0700

    npm install

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
 function App() {
-  return <div className="container"></div>;
+  return <div className="container">
+    
+  </div>;
 }
 
 export default App;
