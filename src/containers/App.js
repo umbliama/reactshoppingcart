10c7b01c57077ea3016db61ae1ccb75424d69b90
Maxim Umblia <umblia34@gmail.com>
2020-09-29 14:53:42 +0700

    added containers

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,31 @@
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import * as bookActions from "../actions/books";
-import * as filterActions from "../actions/filter";
 import App from "../components/App";
+import orderBy from "lodash/orderBy";
+
+const sortBy = (books, filterBy) => {
+  switch (filterBy) {
+    case "all":
+      return books;
+    case "price_high":
+      return orderBy(books, "price", "desc");
+    case "price_high":
+      return orderBy(books, "price", "asc");
+    case "price_low":
+      return orderBy(books, "price", "asc");
+    case "author":
+      return orderBy(books, "author", "asc");
+    default:
+      return books;
+  }
+};
 
 const mapStateToProps = ({ books }) => ({
-  books: books.items,
+  books: sortBy(books.items, books.filterBy),
   isReady: books.isReady,
 });
 const mapDispatchToProps = (dispatch) => ({
   ...bindActionCreators(bookActions, dispatch),
-  ...bindActionCreators(filterActions, dispatch),
 });
 export default connect(mapStateToProps, mapDispatchToProps)(App);
